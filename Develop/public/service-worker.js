5f1c3d28a10c127d99980bbfd566d9fff07ec4fc
louisvincelli <79131518+louisvincelli@users.noreply.github.com>
2021-08-06 17:43:12 -0700

    finalc

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,7 +1,8 @@
+
 var CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-var urlsToCache = [
+var CACHEITEMS = [
   "/",
   "/db.js",
   "/index.js",
@@ -15,7 +16,7 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+      return cache.addAll(CACHEITEMS);
     })
   );
 });
